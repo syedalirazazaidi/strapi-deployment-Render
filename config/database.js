402ecfa9eb8f10c8cc9bd27c1f23ad26c2db2211
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-25 14:06:35 +0500

    changes some databsefile

--- config/database.js
+++ config/database.js
@@ -61,18 +61,19 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true,
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: env.bool('DATABASE_SSL', true),
+        // {
+        //   key: env('DATABASE_SSL_KEY', undefined),
+        //   cert: env('DATABASE_SSL_CERT', undefined),
+        //   ca: env('DATABASE_SSL_CA', undefined),
+        //   capath: env('DATABASE_SSL_CAPATH', undefined),
+        //   cipher: env('DATABASE_SSL_CIPHER', undefined),
+        //   rejectUnauthorized: env.bool(
+        //     'DATABASE_SSL_REJECT_UNAUTHORIZED',
+        //     true,
+        //   ),
+        // },
+        // schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: {
         min: env.int('DATABASE_POOL_MIN', 2),
